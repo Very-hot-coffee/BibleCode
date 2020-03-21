92640e0396a890c8ce1be70e01e51116118c1891
Very-Hot-Cofe <60708898+Very-Hot-Coffe@users.noreply.github.com>
2020-03-22 01:45:32 +0300

    first

---
+++ Bible code/BibleCodeReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bible_code
+{
+    public class BibleCodeReader
+    {
+        public BibleCodeReader(string arg , string exitWay , params int [] array){
+            path = arg;
+            chars.AddRange(array);
+            exitPath = exitWay;
+
+            Console.WriteLine($"Created BibleCodeReader {arg} to {exitPath} ");
+
+            file = File.ReadAllText(arg, System.Text.Encoding.Default);
+        }
+        public string path { get; }
+        public List<int> chars { get; } = new List<int>();
+
+        private string file;
+        private string exitPath;
+
+        public void Read() {
+            Task[] tasks = new Task[chars.Count];
+
+            for(int i = 0; i < tasks.Length; i++) {
+                tasks[i] = ReadFile(chars[i]);
+                //tasks[i].Start();
+            }
+            Task.WaitAll();
+        }
+
+        private async Task ReadFile(int index) {
+            await Task.Run(()=> {
+                if (index > 1)
+                {
+
+                    Console.WriteLine($"\nStart read from index = {index} ...");
+
+                    var result = "";
+                    var count = index;
+                    var exitFilePath = exitPath +
+                        path.Split("\\".ToCharArray()[0]).Last().Split('.')[0] + // Вычленяем название файла
+                        "_resultate_to_index_" + index + ".txt";
+
+                    while (count < file.Length)
+                    {
+                        result += file[count];
+                        count += index;
+                    }
+                    Console.WriteLine($"Save {exitFilePath}");
+                    File.WriteAllText(exitFilePath, result, System.Text.Encoding.Default);
+                    Console.Beep();
+                    Console.WriteLine($"Finished susseful");
+                }
+            });
+            
+        }
+    }
+}
---
+++ Bible code/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bible_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Библейский код, также известный, " +
+                "как код Торы — представление о тексте Торы как о коде," +
+                " содержащем скрытые смыслы (более глубокое понимание " +
+                "прямого текста) и предсказания[1]. Точка зрения," +
+                " согласно которой библейский код содержит пророческую " +
+                "информацию, рассматривается в каббале. \n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n Важное сообщение: данная программа не предсказывает ничего, никакого смысла в полученном тексте нет( автором не задумывалось)," +
+                "это всего лишь теория больших чисел - вероятность осмысленных сочетаний букв достигается из-за большого объёма текста. \n");
+
+            Console.ResetColor();
+
+            if (args.Length > 0)
+            {
+                new BibleCodeReader(args[0], "D:\\", 5).Read();
+            }
+            else {
+                Console.WriteLine("Input file path : ");
+                var path = Console.ReadLine();
+                Console.WriteLine("Input exit file path : ");
+                var exitPath = Console.ReadLine();
+                Console.WriteLine("Input char numers to parce : ");
+                var n = Console.ReadLine().Split(' ');
+                int[] nums = new int[n.Length];
+                for (int i = 0; i < nums.Length; i++) {
+                    int.TryParse(n[i], out nums[i]);
+                }
+
+                new BibleCodeReader(path , exitPath , nums).Read();
+            }
+            Console.Read();
+        }
+    }
+}
